5474c2fd2bf15abdb187ca5d0ecec8ece8087d24
Aman Dhar <aman.dhar@gmail.com>
2017-09-17 15:44:16 -0700

    Completed hw 1.

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.map(&:to_i)
+    a = a.map {|x| x+2}
+    a = a.delete_if {|x| x % 2 == 1}
+    a = a.uniq
+    a = a.delete_if {|x| x > 10}
+    return a.sum
   end
 end
 
